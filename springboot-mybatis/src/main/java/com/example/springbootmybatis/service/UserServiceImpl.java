1214349048dc8ed20c4b58c4a8ca5bafea4d270f
SHICAN <SHICAN@DESKTOP-F0DBC4K>
2018-03-04 18:46:06 +0800

    init

--- springboot-mybatis/src/main/java/com/example/springbootmybatis/service/UserServiceImpl.java
+++ springboot-mybatis/src/main/java/com/example/springbootmybatis/service/UserServiceImpl.java
@@ -16,7 +16,7 @@ public class UserServiceImpl {
 	
 	public List<User> findAll(){
         //将参数传给这个方法就可以实现物理分页了，非常简单。
-        PageHelper.startPage(2, 2);  //test    设定为第一页  && 分页大小为2 
+        PageHelper.startPage(2, 2);  //test    设定为第二页  && 分页大小为2 
 		return userMapper.findAll();
 	}
 
